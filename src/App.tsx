c650a9795f364926a4f831c88fd0de86e23ddd2f
marksapiandante <marksapiandante@gmail.com>
2024-04-04 12:48:41 +0000

    mutating data using useMutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import PostList from "./react-query/PostList";
-// import TodoList from "./react-query/TodoList";
+import TodoForm from "./react-query/TodoForm";
+// import PostList from "./react-query/PostList";
+import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <PostList />
-  // <TodoList />;
+  return (
+  <>
+  <TodoForm />
+  <TodoList />
+  </>)
 }
 
 export default App;
